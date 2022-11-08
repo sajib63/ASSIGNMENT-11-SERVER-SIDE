016fa56e55bf31226b6fc0eea360ab06bef5f127
Sajib Ahmed <sajib7315@gmail.com>
2022-11-08 18:00:41 +0600

    adding dynamic api

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use(express.json());
 
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.z2qhqgi.mongodb.net/?retryWrites=true&w=majority`;
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
@@ -23,13 +23,27 @@ async function run(){
     try{
 
         const serviceCollection=client.db('wildPhoto').collection('photos');
-
+// Read 
         app.get('/services', async(req, res)=>{
             const query={};
+            const cursor=await serviceCollection.find(query);
+            const service= await cursor.limit(3).toArray();
+            res.send(service);
+        })
 
+
+        app.get('/totalServices', async(req, res)=>{
+            const query={};
             const cursor=await serviceCollection.find(query);
             const service= await cursor.toArray();
-            
+            res.send(service);
+        })
+
+         
+        app.get('/totalServices/:id', async(req, res)=>{
+            const id=req.params.id;
+            const query={_id: ObjectId(id)};
+            const service=await serviceCollection.findOne(query);
             res.send(service);
         })
 

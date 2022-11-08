374d32479f85b1636244ae1b3d77520fc0ed859c
Sajib Ahmed <sajib7315@gmail.com>
2022-11-08 16:49:11 +0600

    adding read link

--- index.js
+++ index.js
@@ -18,6 +18,32 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@clu
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 
+async function run(){
+
+    try{
+
+        const serviceCollection=client.db('wildPhoto').collection('photos');
+
+        app.get('/services', async(req, res)=>{
+            const query={};
+
+            const cursor=await serviceCollection.find(query);
+            const service= await cursor.toArray();
+            
+            res.send(service);
+        })
+
+    }
+
+    finally{
+
+    }
+
+}
+run().catch(error=>{
+    console.log(error);
+})
+
 
 
 

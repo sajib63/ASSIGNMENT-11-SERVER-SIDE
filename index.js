9b2e55031b170263e7c4a13ecb597eff56ae2687
Sajib Ahmed <sajib7315@gmail.com>
2022-11-08 16:09:33 +0600

    adding mongodb

--- index.js
+++ index.js
@@ -12,6 +12,14 @@ app.use(express.json());
 
 
 
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.z2qhqgi.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+
+
 
 app.get('/', (req, res) => {
   res.send('Hello assignment server')
